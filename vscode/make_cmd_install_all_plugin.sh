be628061f6d90e6dd5aac1b0e3fffd3ab278f5c7
paikwiki <paikwiki@gmail.com>
2020-03-15 21:24:27 +0900

    Remove previous shell scripts

--- vscode/make_cmd_install_all_plugin.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-code --list-extensions | xargs -L 1 echo code --install-extension > install-all-extensions.sh
--- vscode/make_cmd_uninstall_all_plugin.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-code --list-extensions | xargs -L 1 echo code --uninstall-extension > uninstall-all-extensions.sh
