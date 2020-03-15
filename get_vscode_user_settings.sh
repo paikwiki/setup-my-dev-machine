23bbdb1a041fd780c36552c0b05fe9e856c5b266
paikwiki <paikwiki@gmail.com>
2020-03-15 22:07:05 +0900

    Create get_vscode_user_settings.sh

---
+++ get_vscode_user_settings.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ -r userconfig.sh ]; then
+	. ./userconfig.sh
+	if [ $dir_user_root ]; then
+		dir_vscode_user="${dir_user_root}/Library/Application Support/Code/User"
+
+		cat "${dir_vscode_user}/settings.json" \
+				> ./vscode/settings.json
+		cnt=$(cat "./vscode/settings.json" | wc -l | tr -d " ")
+		printf "$cnt lines was written.\n"
+	else
+		RED='\033[0;31m'
+		DEFAULT='\033[0;0m'
+		printf "${RED}\$dir_user_root${DEFAULT} is undefined. Check userconfig.sh.\n"
+	fi
+else
+	printf "${RED}userconfig.sh${DEFAULT} does not exist. Create userconfig.sh with userconfig.sh.sample.\n"
+fi
